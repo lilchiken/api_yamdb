4123eba084854a18f714743e77bda27762a11963
localmachine <localmachine@Airlocalmachine.Dlink>
2023-03-29 01:36:16 +0300

    create user model and one custom permission

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Обычный юзер джанго,
+    но с двумя дополнительными полями ("role", "bio").
+    """
+
+    ROLE_CHOICES = [
+        ('user', 'user'),
+        ('moderator', 'moderator'),
+        ('admin', 'admin')
+    ]
+
+    bio = models.TextField(
+        verbose_name='Биография',
+        blank=True
+    )
+    role = models.CharField(
+        verbose_name='Роль',
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
