f13047672140c9f0a9439d628482f77a1110d354
itsqntz <itsqntz@ya.ru>
2023-03-29 21:48:39 +0300

    First release

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,58 @@
-from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+User = get_user_model()
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    score = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        default_related_name = 'reviews'
+        ordering = ['pub_date']
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        default_related_name = 'comments'
+        ordering = ['pub_date']
