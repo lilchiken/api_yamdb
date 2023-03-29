80e6f0267b2da61a0ceea389eba19c65c1a1aa9c
Михаил Матвеев <wushusanshou@mail.ru>
2023-03-29 22:16:59 +0300

    Added title, genre, categories models, urls, serializers, views

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+
+from api.views import TitlesViewSet, CategoriesViewSet, GenresViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register('titles', TitlesViewSet, basename='titles')
+router_v1.register('genres', GenresViewSet, basename='genres')
+router_v1.register('categories', CategoriesViewSet, basename='categories')
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+    path('v1/', include('djoser.urls.jwt')),
+]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,69 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        blank=False,
+        unique=True,
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        blank=False,
+        unique=True,
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.TextField(
+        blank=False,
+        max_length=256,
+    )
+    year = models.PositiveIntegerField
+    description = models.TextField(
+        'Описание',
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='TitleGenre',
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='category'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class TitleGenre(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE
+    )
